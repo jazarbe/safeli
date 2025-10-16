a45402274567967fc3c92266f2f7f398b48daf26
JAZMIN ARIAS BERLIN <49308713@ort>
2025-10-16 15:55:04 -0300

    view destinos terminada

--- Models/Orbit.cs
+++ Models/Orbit.cs
@@ -5,7 +5,14 @@ public class Orbit
     public int id {get; private set;}
     public string name{get; private set;}
     public string foto{get; private set;}
+    public List<Usuario> usuarios {get; private set;}
 
     public Orbit(){}
+    public Orbit(string name, string foto, List<Usuario> usuarios)
+{
+    this.name = name;
+    this.foto = foto;
+    this.usuarios = usuarios;
+}
 
 }
