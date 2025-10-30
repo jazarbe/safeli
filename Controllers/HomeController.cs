96d7b19067ac7ecee11e10a2ffeeac9b7ba93030
DIANA IRENE PARK <49377760@ort>
2025-10-30 10:35:09 -0300

    hackaton 2 bloque, intento de views --> no terminado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,7 +25,17 @@ public class HomeController : Controller
 
      public IActionResult Destinos()
     {
+        ViewBag.Contacto = HttpContext.Session.GetString("Contacto");
+        ViewBag.Nombre = HttpContext.Session.GetString("NombreContacto");
         return View();
+    }
+
+     [HttpPost]
+    public IActionResult GuardarContacto(string nombre, string numero)
+    {
+        HttpContext.Session.SetString("NombreContacto", nombre);
+        HttpContext.Session.SetString("Contacto", numero);
+        return RedirectToAction("Destinos");
     }
 
     public IActionResult Home()
